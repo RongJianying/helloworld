780a7339fa819a62c9f250fdb4a8bf174b79f4e4
RongJianying <jyrong@freewheel.tv>
2019-03-31 20:51:20 +0800

    REST

--- rails_project/demo/app/controllers/events_controller.rb
+++ rails_project/demo/app/controllers/events_controller.rb
@@ -3,6 +3,12 @@ class EventsController < ApplicationController
 
   def index
     @events = Event.page(params[:page]).per(5)
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml { render :xml => @events.to_xml }
+      format.json { render :json => @events.to_json }
+      format.atom { @feed_title = "My event list" } # index.atom.builder
+    end
   end
 
   def new
@@ -13,14 +19,20 @@ class EventsController < ApplicationController
     flash[:notice] = "event was successfully created"
     @event = Event.new(event_params)
     if @event.save
-      redirect_to :action => :index
+      redirect_to events_url
     else
       render :action => :new
     end
   end
 
   def show
-    @page_title = @event.name
+    Rails.logger.debug("event: #{@event.inspect}")
+    @event = Event.find(params[:id])
+    respond_to do |format|
+      format.html { @page_title = @event.name } # show.html.erb
+      format.xml # show.xml.builder
+      format.json { render :json => { id: @event.id, name: @event.name }.to_json }
+    end
   end
 
   def edit
@@ -29,7 +41,7 @@ class EventsController < ApplicationController
   def update
     flash[:notice] = "event was successfully updated"
     if @event.update(event_params)
-      redirect_to :action => :show, :id => @event
+      redirect_to events_url(@event)
     else
       render :action => :edit
     end
@@ -39,7 +51,7 @@ class EventsController < ApplicationController
     flash[:alert] = "event was successfully deleted"
     @event.destroy
 
-    redirect_to :action => :index
+    redirect_to events_url
   end
 
   private
--- rails_project/demo/config/routes.rb
+++ rails_project/demo/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :people
+  resources :events
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "welcome/say_hello" => "welcome#say"
   get "welcome" => "welcome#index"
-  match ':controller(/:action(/:id(.:format)))', :via => :all
+  # match ':controller(/:action(/:id(.:format)))', :via => :all
 end
